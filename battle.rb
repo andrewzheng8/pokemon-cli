6434d0a8efe2dbc4fb0596286c40993f4177622b
anneeb <ahuang852@gmail.com>
2017-05-08 23:30:09 -0400

    remove stab because we're not using attack types

--- battle.rb
+++ battle.rb
@@ -1,17 +1,11 @@
 
 def damange(atker, defer)
   random = (0.85..1.00)
-  mods = random * stab * type
+  mods = random * type
   damage = (((2 * atker[:level] / 5) + 2) * (atker[:attack] / defer[:defense]) / 50 + 2) * mods
 
 def random
   random = (0.85..1.00)
-end
-
-def stab(atker, defer)
-  stab = 1.00
-  stab = 1.50 if atker[:type] == defer[:type]
-  stab
 end
 
 def type(atker, defer)
